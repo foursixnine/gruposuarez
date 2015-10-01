bd0f40b92fa08bae0d08eb1033b6e5ecd8959aa2
Gabriela Ilarreta <gabrielailarreta@gmail.com>
2015-10-01 11:14:59 -0500

    Cambio en fecha permiso de ocupacion

--- protected/views/gestion/morosidadproyecto.php
+++ protected/views/gestion/morosidadproyecto.php
@@ -12,7 +12,7 @@ $sumtv=0;
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
                 <meta http-equiv="Content-Language" content="pt-br, pt">
                  
-
+ 
 
 </head>
 <table border="1">
