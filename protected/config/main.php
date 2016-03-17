9c27af384321ed59b7b05185b81242c80a813dec
Gabriela Ilarreta <gabrielailarreta@gmail.com>
2016-03-17 11:29:52 -0500

    Cambio fecha tramite

--- protected/config/main.php
+++ protected/config/main.php
@@ -98,7 +98,7 @@ return array(
 			),
 		),
                 //
-		// database settings areee configured in database.php*/
+		// database settings areee configdddured in database.php*/
                 'db'=>array(
                         'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
                 ),
