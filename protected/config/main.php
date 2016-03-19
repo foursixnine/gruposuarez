afcccf5b4f749c78be85359f153c8ea33adc87f3
Gabriela Ilarreta <gabrielailarreta@gmail.com>
2016-03-19 08:38:02 -0500

    Numero de Tramite

--- protected/config/main.php
+++ protected/config/main.php
@@ -105,9 +105,9 @@ return array(
                 //Segunda Conexion  
                 'dbconix'=>array(
 						'class' => 'CDbConnection',
-						'connectionString' => 'mysql:host=186.74.216.58;dbname=enx_suarez',
+						//'connectionString' => 'mysql:host=186.74.216.58;dbname=enx_suarez',
 						'enableProfiling' => YII_DEBUG_PROFILING,
-					   // 'connectionString' => 'mysql:host=192.168.0.159;dbname=enx_suarez',
+					    'connectionString' => 'mysql:host=192.168.0.159;dbname=enx_suarez',
 						'emulatePrepare' => true,
 						'username' => 'suarez',
 						'password' => '!suarez2015!',
