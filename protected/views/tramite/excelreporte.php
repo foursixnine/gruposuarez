0fef93eaf01443d35add6fc4ed07f999edaa4f2a
Gaby Sony <gilarreta@valorca.com>
2016-04-04 21:46:17 -0500

    Index CLientes

--- protected/views/tramite/excelreporte.php
+++ protected/views/tramite/excelreporte.php
@@ -18,7 +18,7 @@ if($data!==null){
 
 <td colspan="15" color="">
     <br/>
-<div style='background-color:#CCC'><STRONG>REPORTE</STRONG></div>
+<div style='background-color:#CCC'><STRONG>REPORTE STATUS ACTUAL</STRONG></div>
     <BR>
    
   <p><h2>GRUPO SUAREZ</h2></p>
@@ -72,8 +72,8 @@ if($data!==null){
                 <td><?php echo $value['agente_tramite'];?></td>
                 <td><?php echo $value['vendedor']; ?></td>
                 <td><?php echo $value['nombre_de_empresa'];?></td>              
-                <td>$<?php echo $value['total_venta'];?></td>
-                <td>$<?php echo $value['monto_liquidacion'];?></td>
+                <td><?php echo $value['total_venta'];?></td>
+                <td><?php echo $value['monto_liquidacion'];?></td>
                 <td><?php if($value['banco_acreedor']=="S/DESCRIPCION"){
                             echo "DE CONTADO"; 
                 }else{
